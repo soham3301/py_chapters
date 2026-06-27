d8aff4170981f9b0f40ac2c6145114701a15fd2c
Soham Datta <splint3r777@gmail.com>
2026-06-27 15:43:21 +0530

    little while & function

---
+++ sec6/functions.py
@@ -0,0 +1,12 @@
+# The function body
+def my_function():
+    print("I just created a function")
+
+my_function()
+
+# Function with input
+def another_function(num1, num2):
+    result = num1 + num2
+    print(result)
+
+another_function(20, 30)
---
+++ sec6/while_loop.py
@@ -0,0 +1,13 @@
+# This is while loop
+
+stopper = 10
+while stopper > 0:
+    stopper -= 1
+    if stopper == 6:
+        print("I don't like 6")
+        continue
+    print(f"The value of Stopper is: {stopper}")
+
+# While (something is True):
+#   Run Your Code
+
