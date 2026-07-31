423bd20b2efd4f01c39c57964e7797d3bc588c9f
Soham Datta <splint3r777@gmail.com>
2026-07-31 21:15:40 +0530

    July End Push

--- 29_oop_intro.py
+++ 29_oop_intro.py
@@ -1,14 +1,65 @@
 import turtle
 import tkinter as TK
 import prettytable
+import random
 
 raphael = turtle.Turtle()
 my_screen = turtle.Screen()
 
+my_screen.bgcolor("black")
+
 raphael.shape("turtle")
-raphael.color("red")
+raphael.color("SpringGreen")
+raphael.pensize(15)
+
+
+table = prettytable.PrettyTable()
+
+
+
+
+directions = [0, 90 ,180, 270]
+
+for _ in range(200):
+    raphael.forward(30)
+    raphael.setheading(random.choice(directions))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 raphael.forward(100)
 my_screen.exitonclick()
-
-table = prettytable.PrettyTable()
