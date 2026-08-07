1dbc743ca565288a6b9a5f6ce4804bdb9adcc459
Soham Datta <splint3r777@gmail.com>
2026-08-07 23:27:43 +0530

    csv and pandas introduction - refactored

--- day_25_csv_pandas/main.py
+++ day_25_csv_pandas/main.py
@@ -1,87 +1,85 @@
 
-# #? Reading CSV with [with open()] method
-# # with open("weather_data.csv") as weather:
-# #     data = weather.readlines()
-# #     print(data)
+#? Reading CSV with [with open()] method
+with open("weather_data.csv") as weather:
+    data = weather.readlines()
+    print(data)
 
 
-# #? Reading CSV with [import python inbuilt csv] method
-# # import csv
+#? Reading CSV with [import python inbuilt csv] method
+import csv
 
-# # with open("weather_data.csv") as data_file:
-# #     data = csv.reader(data_file)
-# #     print(data)
-# #     temperature = []
-# #     for row in data:
-# #         if row[0] != "day":
-# #             temperature.append(int(row[1]))
-# #     print(temperature)
-
-
-
-# #? Reading CSV with [import pandas library] method
-# import pandas
-
-# data = pandas.read_csv("weather_data.csv")
-# # data_dict = data.to_dict()
-# # temperature_series = data["temp"]
-# # data_series = temperature_series.to_list()
-
-# # total_temp = 0
-# # for temp in data_series:
-# #     total_temp += temp
-
-# # avg_temp = round(total_temp / len(data_series))
-# # print(f"Average Temperature: {avg_temp}")
-
-# # max_temp = temperature_series.max()
-# # print(f"Maximum Temperature: {max_temp}")
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    print(data)
+    temperature = []
+    for row in data:
+        if row[0] != "day":
+            temperature.append(int(row[1]))
+    print(temperature)
 
 
-# #* Get Data in columns
-# # print(data["condition"])
-# # print(data.condition)
 
-# #* Get Data in row
-# # print(data[data["day"] == "Monday"])
-# # print(data[data.day == "Thursday"])
+#? Reading CSV with [import pandas library] method
+import pandas
 
+data = pandas.read_csv("weather_data.csv")
+data_dict = data.to_dict()
+temperature_series = data["temp"]
+data_series = temperature_series.to_list()
 
-# #* Find the row where temperature is maximum
-# # print(data[data.temp == data.temp.max()])
+total_temp = 0
+for temp in data_series:
+    total_temp += temp
 
-# #* Tapping row values
-# monday = data[data.day == "Monday"]
+avg_temp = round(total_temp / len(data_series))
+print(f"Average Temperature: {avg_temp}")
 
-# monday.day
-# monday_temp = monday.temp[0]
-# monday.condition
-
-# #* Get monday Temperature in Farenhiet
-# temp_in_farenhiet = round(monday_temp * 9/5) + 32
-# # print(f"Temperature in Farenhiet: {temp_in_farenhiet}")
+max_temp = temperature_series.max()
+print(f"Maximum Temperature: {max_temp}")
 
 
-# #* Create a dataframe from scratch
+#* Get Data in columns
+print(data["condition"])
+print(data.condition)
 
-# # student_dict = {
-# #     "students":["Soham", "Amitabh", "Akash"],
-# #     "score":[67, 77, 98],
-# # }
+#* Get Data in row
+print(data[data["day"] == "Monday"])
+print(data[data.day == "Thursday"])
 
-# # student_data = pandas.DataFrame(student_dict)
-# # print(student_data)
 
-# # student_data.to_csv("student_data.csv")
+#* Find the row where temperature is maximum
+print(data[data.temp == data.temp.max()])
 
-# new_student_data = pandas.read_csv("student_data.csv")
+#* Tapping row values
+monday = data[data.day == "Monday"]
 
-# print(new_student_data)
+monday.day
+monday_temp = monday.temp[0]
+monday.condition
+
+#* Get monday Temperature in Farenhiet
+temp_in_farenhiet = round(monday_temp * 9/5) + 32
+print(f"Temperature in Farenhiet: {temp_in_farenhiet}")
+
+
+#* Create a dataframe from scratch
+
+student_dict = {
+    "students":["Soham", "Amitabh", "Akash"],
+    "score":[67, 77, 98],
+}
+
+student_data = pandas.DataFrame(student_dict)
+print(student_data)
+
+student_data.to_csv("student_data.csv")
+
+new_student_data = pandas.read_csv("student_data.csv")
+
+print(new_student_data)
 
 
 #? ============================ SQUIRREL DATA ============================
-
-import pandas
 
 squirrel_data  =pandas.read_csv("squirrel_data.csv")
 
