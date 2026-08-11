2fadff585781bf08207d20c310cfd30d53f3d91e
Soham Datta <splint3r777@gmail.com>
2026-08-11 17:11:28 +0530

    Tried args and kwargs

---
+++ day_28_args_kwargs/args.py
@@ -0,0 +1,10 @@
+
+def function(*args):
+    print(args)
+
+function("hello", "I am soham", 24, "Years old")
+
+def k_fucn(**kwargs):
+    print(kwargs)
+
+k_fucn(name="Soham", age=34, job="freelancer")
